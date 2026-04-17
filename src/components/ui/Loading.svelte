ca7e9a72dd06a3f3cd5490e7706222ada83996a6
Benjamin Rasmussen <rasben@users.noreply.github.com>
2026-04-17 17:24:25 +0200

    Basic loader

---
+++ src/components/ui/Loading.svelte
@@ -0,0 +1,7 @@
+<script>
+import {Loader} from "$lib/components/ui/loader/index.ts";
+</script>
+
+<h3 class="relative z-1 p-8 text-4xl font-outline text-center flex justify-center content-end items-end">
+    Loading <Loader class="mx-2" />
+</h3>
