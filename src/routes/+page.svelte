d0a75945d8ee0e664b8491726fba530af0528db2
Benjamin Rasmussen <rasben@users.noreply.github.com>
2026-04-17 17:26:12 +0200

    Less borders on the tab display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,16 @@
   import Main from "../components/Main.svelte";
 </script>
 
-<main class="p-8">
-  <Tabs.Root value="main">
+<main class="flex min-h-screen flex-col p-8">
+  <Tabs.Root value="main" class="min-h-0 min-w-0 flex-1">
     <Tabs.List>
       <Tabs.Trigger value="main">Main</Tabs.Trigger>
       <Tabs.Trigger value="settings">Settings</Tabs.Trigger>
     </Tabs.List>
-    <Tabs.Content value="main" class="!border-0 !p-0 mt-4">
+    <Tabs.Content value="main" class="mt-4 flex min-h-0 flex-col !border-0 !p-0">
       <Main />
     </Tabs.Content>
-    <Tabs.Content value="settings" class="!border-0 !p-0 mt-4">
+    <Tabs.Content value="settings" class="mt-4 flex min-h-0 flex-col !border-0 !p-0">
       <Settings />
     </Tabs.Content>
   </Tabs.Root>
